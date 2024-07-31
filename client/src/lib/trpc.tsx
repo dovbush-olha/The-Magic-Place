1b611c4115f23fc0efdc881fd86df03206fda260
olha-dovbush <olha.dovbush@jito.dev>
2024-07-31 16:42:45 +0300

    Create trcpProvider for the client

--- client/src/lib/trpc.tsx
+++ client/src/lib/trpc.tsx
@@ -1,4 +1,30 @@
-import { createTRPCReact } from '@trpc/react-query';
-import type { AppRouter } from '../../../server/src/trpc.ts';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { createTRPCReact, httpBatchLink } from '@trpc/react-query';
+
+import type { AppRouter } from '../../../server/src/trpc';
 
 export const trpc = createTRPCReact<AppRouter>();
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+const trpcClient = trpc.createClient({
+  links: [httpBatchLink({ url: 'http://localhost:3000/trcp' })],
+});
+
+export const trpcProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <trpc.Provider
+      client={trpcClient}
+      queryClient={queryClient}
+    >
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </trpc.Provider>
+  );
+};
