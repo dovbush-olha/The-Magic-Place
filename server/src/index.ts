72dbfed7822c9a04a31bdfd96054e93daae6698e
olha-dovbush <olha.dovbush@jito.dev>
2024-08-01 14:08:43 +0300

    Use cors with express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import { appRouter } from './trpc';
 import * as trpcExpress from '@trpc/server/adapters/express';
 
 const expressApp = express();
+expressApp.use(cors());
 expressApp.use('/api', trpcExpress.createExpressMiddleware({ router: appRouter }));
 
 expressApp.listen('3000', () => {
