d15abae744d2b4c088dc1aefd734e1d452b0dfe2
olha-dovbush <olha.dovbush@jito.dev>
2024-08-05 11:30:23 +0300

    feat: Create ViewIdeaPage template

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { ServerApiClientProvider } from './lib/trpc.tsx';
 import { AllIdeasPage } from './pages/AllIdeasPage';
+import { ViewIdeaPage } from './pages/ViewIdeaPage';
 
 export function App() {
   return (
     <ServerApiClientProvider>
       <AllIdeasPage />
+      <ViewIdeaPage />
     </ServerApiClientProvider>
   );
 }
---
+++ client/src/pages/ViewIdeaPage/index.tsx
@@ -0,0 +1,13 @@
+export function ViewIdeaPage() {
+  return (
+    <div>
+      <h1>Idea 1</h1>
+      <p>Description of idea 1...</p>
+      <div>
+        <p>Text paragraph 1 for idea 1</p>
+        <p>Text paragraph 2 for idea 1</p>
+        <p>Text paragraph 3 for idea 1</p>
+      </div>
+    </div>
+  );
+}
