a5936daa7af4873ec2b7f2889211f46c9ee7e70d
olha-dovbush <olha.dovbush@jito.dev>
2024-08-27 10:44:16 +0300

    feat: create starsOptions object

---
+++ client/src/lib/particles/starsOptions.ts
@@ -0,0 +1,27 @@
+export const starsOptions = {
+  preset: 'stars',
+  background: {
+    color: '#000616',
+  },
+  particles: {
+    number: {
+      value: 2590,
+    },
+    move: {
+      enable: true,
+      directions: 'none',
+      speed: 0.08,
+      random: false,
+      straight: false,
+    },
+    color: {
+      value: '#717981',
+    },
+    collisions: {
+      enable: true,
+    },
+    size: {
+      value: { min: 0.01, max: 1.4 },
+    },
+  },
+};
