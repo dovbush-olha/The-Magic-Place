c5a14d916e3e9349c2096ad2b35acae5fed90cc4
olha-dovbush <olha.dovbush@jito.dev>
2024-08-28 15:46:26 +0300

    feat: create primary-font variable and use variables inside global.scss file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
+import '@fontsource-variable/geologica';
+
 import { appRoutes } from './lib/routes/router';
 import { ServerApiClientProvider } from './lib/trpc.tsx';
+
+import './scss/global.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
