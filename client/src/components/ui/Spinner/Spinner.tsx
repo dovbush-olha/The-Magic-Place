8d4caf8cbefaaf6c580f98dce0e76790bf33a2d5
olha-dovbush <olha.dovbush@jito.dev>
2024-08-29 17:28:59 +0300

    feat: position Spinner always in center

--- client/src/components/ui/Spinner/Spinner.tsx
+++ client/src/components/ui/Spinner/Spinner.tsx
@@ -2,10 +2,12 @@ import styles from './Spinner.module.scss';
 
 export function Spinner() {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.firstRing} />
-      <div className={styles.secondRing} />
-      <div className={styles.thirdRing} />
+    <div className={styles.center}>
+      <div className={styles.wrapper}>
+        <div className={styles.firstRing} />
+        <div className={styles.secondRing} />
+        <div className={styles.thirdRing} />
+      </div>
     </div>
   );
 }
