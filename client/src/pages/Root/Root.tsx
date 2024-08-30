47e8fef9589d414a71a6a1e715e5eb3f39a4fcb6
olha-dovbush <olha.dovbush@jito.dev>
2024-08-30 15:54:51 +0300

    feat: configure absolute imports for client

--- client/src/pages/Root/Root.tsx
+++ client/src/pages/Root/Root.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Particles from '@tsparticles/react';
 
-import { starsOptions } from '../../lib/particles/starsOptions.ts';
+import { starsOptions } from '@/lib/particles/starsOptions.ts';
 
 import { useStarsParticles } from './useStarsParticles.ts';
 
