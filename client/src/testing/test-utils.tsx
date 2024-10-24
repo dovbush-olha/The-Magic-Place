02da46d26aa25d4aba55fa5383e50a660b007298
olha-dovbush <olha.dovbush@jito.dev>
2024-10-24 10:29:10 +0300

    refactor: export userEvent from test-utils.tsx file

--- client/src/testing/test-utils.tsx
+++ client/src/testing/test-utils.tsx
@@ -1,10 +1,11 @@
 import { ReactNode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { render, RenderOptions, RenderResult } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 function renderWithRouter(ui: ReactNode, options?: Omit<RenderOptions, 'wrapper'>): RenderResult {
   return render(ui, { wrapper: BrowserRouter, ...options });
 }
 
 export * from '@testing-library/react';
-export { renderWithRouter };
+export { renderWithRouter, userEvent };
