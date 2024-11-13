299186e6994445f837523f914ab17661dec33cfd
olha-dovbush <olha.dovbush@jito.dev>
2024-11-13 16:30:29 +0200

    feat: create Container component

---
+++ client/src/components/layouts/container/container.stories.tsx
@@ -0,0 +1,18 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { Container } from './container';
+
+const meta: Meta<typeof Container> = {
+  component: Container,
+  title: 'Layouts/Container',
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Container>;
+
+export const Default: Story = {
+  args: {
+    children: 'Container',
+  },
+};
